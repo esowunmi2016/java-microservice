707370934e259af7d6ef2e34d9ce69c9094908fc
esowunmi2016 <esowunmi2016@fau.edu>
2024-09-14 22:42:12 -0400

    .

--- testserver/src/main/java/com/example/controller/Controller.java
+++ testserver/src/main/java/com/example/controller/Controller.java
@@ -1,2 +1,17 @@
-package com.example.controller;public class Contollrt {
+package com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Service
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public ResponseEntity<String> tst(){
+        System.out.println("Hello from test server");
+        return new ResponseEntity<String>("Nacho cheese", HttpStatus.valueOf(200));
+    }
 }
